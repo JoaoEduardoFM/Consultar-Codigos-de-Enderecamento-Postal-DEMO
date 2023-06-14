a15820584e646e0dea2a0ead132c8e8fcb31761d
João Martins <90796699+JoaoEduardoFM@users.noreply.github.com>
2023-06-14 01:00:41 -0300

    identado classe Cliente controller

--- src/main/java/br/com/controller/ClienteController.java
+++ src/main/java/br/com/controller/ClienteController.java
@@ -26,59 +26,47 @@ import springfox.documentation.annotations.ApiIgnore;
 
 @RestController
 @RequestMapping("/cliente")
-@Api( value = "Cadastra cliente", tags = { "cliente" })
+@Api(value = "Cadastra cliente", tags = { "cliente" })
 public class ClienteController {
-	
+
 	@Autowired
 	ClienteRepository repository;
-	
+
 	@Autowired
 	BuscaCepController controller;
 
 	@GetMapping
-    @ResponseBody 
-	@ApiOperation (
-      value = "Lista contas cadastradas.",
-      notes = "Lista contas vinculadas as contas corrente."
-    )
-    @ResponseStatus(HttpStatus.OK)
-    public Iterable<Cliente> listaCliente(){
-        return repository.findAll();
-    }
-	
+	@ResponseBody
+	@ApiOperation(value = "Lista contas cadastradas.", notes = "Lista contas vinculadas as contas corrente.")
+	@ResponseStatus(HttpStatus.OK)
+	public Iterable<Cliente> listaCliente() {
+		return repository.findAll();
+	}
+
 	@GetMapping("/{id}")
-    @ResponseBody 
-	@ApiOperation (
-      value = "Busca um cliente cadastrado.",
-      notes = "Lista cliente baseado no id."
-    )
-    @ResponseStatus(HttpStatus.OK)
+	@ResponseBody
+	@ApiOperation(value = "Busca um cliente cadastrado.", notes = "Lista cliente baseado no id.")
+	@ResponseStatus(HttpStatus.OK)
 	public ResponseEntity<?> buscarPorId(@PathVariable Long id, @ApiIgnore ResponseRest response) {
-		if(validaSeExisteId(id).equals(false)) {
+		if (validaSeExisteId(id).equals(false)) {
 			response.setMessage("O id informado não existe.");
-        	response.setType(messageType.ATENCAO);    	
-        	return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+			response.setType(messageType.ATENCAO);
+			return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
 		}
 		return ResponseEntity.ok(repository.findById(id));
 	}
-	
+
 	@PostMapping
-    @ResponseBody 
-	@ApiOperation (
-      value = "Cadastra um novo cliente",
-      notes = "cadastra cliente."
-    )
-    @ResponseStatus(HttpStatus.OK)
+	@ResponseBody
+	@ApiOperation(value = "Cadastra um novo cliente", notes = "cadastra cliente.")
+	@ResponseStatus(HttpStatus.OK)
 	public ResponseEntity<Cliente> inserir(@RequestBody Cliente cliente) throws Exception {
 		return ResponseEntity.ok(repository.save(cliente));
 	}
-	
+
 	@PutMapping("/{id}")
-    @ResponseBody 
-	@ApiOperation (
-      value = "Atualiza UM registro",
-      notes = "Atualiza registro relacionado a um cliente."
-    )
+	@ResponseBody
+	@ApiOperation(value = "Atualiza UM registro", notes = "Atualiza registro relacionado a um cliente.")
 	@ResponseStatus(HttpStatus.OK)
 	public ResponseEntity<?> atualiza(@RequestBody Cliente cliente, @ApiIgnore ResponseRest response) {
 		Long id = cliente.getId();
@@ -87,39 +75,36 @@ public class ClienteController {
 			return ResponseEntity.ok(repository.save(cliente));
 		}
 		response.setMessage("O id informado não existe.");
-    	response.setType(messageType.ATENCAO);    	
-    	return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+		response.setType(messageType.ATENCAO);
+		return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
 	}
-	
+
 	@DeleteMapping("/{id}")
-    @ResponseBody 
-	@ApiOperation (
-      value = "Deleta um registro",
-      notes = "Deleta registro relacionado a um cliente."
-    )
+	@ResponseBody
+	@ApiOperation(value = "Deleta um registro", notes = "Deleta registro relacionado a um cliente.")
 	@ResponseStatus(HttpStatus.OK)
 	public ResponseEntity<?> deletar(@PathVariable Long id, @ApiIgnore ResponseRest response) {
 		Optional<?> cliente2 = repository.findById(id);
 		if (cliente2.isPresent()) {
-		repository.deleteById(id);
-		response.setMessage("Registro excluído com sucesso.");
-    	response.setType(messageType.SUCESSO);    	
-    	return new ResponseEntity<>(response, HttpStatus.OK);
+			repository.deleteById(id);
+			response.setMessage("Registro excluído com sucesso.");
+			response.setType(messageType.SUCESSO);
+			return new ResponseEntity<>(response, HttpStatus.OK);
 		} else {
-		response.setMessage("O id informado não existe.");
-    	response.setType(messageType.ATENCAO);    	
-    	return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
-		}		
+			response.setMessage("O id informado não existe.");
+			response.setType(messageType.ATENCAO);
+			return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+		}
 	}
-	
+
 	public Boolean validaSeExisteId(Long id) {
 		Optional<Cliente> cliente = repository.findById(id);
 		try {
-		if(cliente.get().getId() != null) {
-	     return true;
-		}
-		}catch(Exception e) {
-		return false;
+			if (cliente.get().getId() != null) {
+				return true;
+			}
+		} catch (Exception e) {
+			return false;
 		}
 		return false;
 	}
